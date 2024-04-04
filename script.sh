97344c860e1ffcd3573cfa805577f9f87a856cf0
uchitsa <uchitsa@ya.ru>
2024-04-04 16:56:39 +0300

    update script.sh for reading cfp.yml

--- script.sh
+++ script.sh
@@ -1,38 +1,5 @@
 #!/bin/bash
 
 # Parse YAML file using yq
-rows=$(yq e '.events[]' cfp.yml)
+rows=$( yq 'to_entries' cfp.yml)
 echo "$rows"
-
-keys=$(echo rows | jq -r '.[0].event | keys[]')
-
-# Start the Markdown table
-markdown_table="|"
-
-# Add the headers to the Markdown table
-for key in $keys
-do
-    markdown_table+=" $key |"
-done
-
-markdown_table+="\n|"
-
-# Add the separator line
-for key in $keys
-do
-    markdown_table+=" --- |"
-done
-
-# Add the rows to the Markdown table
-for row in $(echo $yaml_data | jq -c '.[]')
-do
-    markdown_table+="\n|"
-    for key in $keys
-    do
-        value=$(echo $row | jq -r ".row.$key")
-        markdown_table+=" $value |"
-    done
-done
-
-# Print the Markdown table
-echo -e $markdown_table
