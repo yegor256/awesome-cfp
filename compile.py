1d753951ffbd85d104849a1a957d278f87a310b2
uchitsa <uchitsa@ya.ru>
2024-04-04 20:29:19 +0300

    update compile.py for prepare markdown

--- compile.py
+++ compile.py
@@ -9,4 +9,12 @@ for x in li:
     print(x.keys())
     print(x.values())
 
-headers = ['publisher', 'CORE', 'Scope', 'Short pages', 'Full pages', 'Format', 'CFP']
+headers = ['name', 'publisher', 'CORE', 'Scope', 'Short pages', 'Full pages', 'Format', 'CFP']
+
+# prepare markdown
+markdown_table = "| " + " | ".join(headers) + " |\n"
+markdown_table += "| " + " | ".join(["---"] * len(headers)) + " |\n"
+print(markdown_table)
+
+with open('generated.md', 'w') as markdown_file:
+    markdown_file.write(markdown_table)
