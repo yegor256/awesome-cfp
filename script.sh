f006178e49f9d4be3f4c74b309d47e28b48a355e
uchitsa <uchitsa@ya.ru>
2024-04-05 18:53:15 +0300

    update script.sh for reading cfp.yml

--- script.sh
+++ script.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
-# Parse YAML file using yq
-rows=$( yq 'to_entries' cfp.yml)
-echo "$rows"
+# Load YAML file
+yaml_content=$(yq eval -o=json cfp.yml)
+
+# Convert JSON to array
+li=("$yaml_content")
+
+# Print keys and values
+for x in "${li[@]}"; do
+    echo $(echo $x | jq 'keys')
+    echo $(echo $x | jq 'values')
+done
+
+# Define headers
+headers=('Name' 'Publisher' 'CORE' 'Scope' 'Short pages' 'Full pages' 'Format' 'CFP')
+
+# Prepare markdown
+markdown_table="| "
+for header in "${headers[@]}"; do
+    markdown_table+="$header | "
+done
+markdown_table+="\n| "
+for header in "${headers[@]}"; do
+    markdown_table+="--- | "
+done
+markdown_table+="\n"
+
+echo $markdown_table
+
+# Write to markdown file
+echo -e $markdown_table > generated.md
