51c51ce5268dacd7e07e28325063ee5c8a839248
uchitsa <uchitsa@ya.ru>
2024-04-05 20:10:58 +0300

    update compile.py for prepare markdown

--- compile.py
+++ compile.py
@@ -5,13 +5,8 @@ with open('cfp.yml', 'r') as stream:
     yaml_content = yaml.safe_load(stream)
     li.append(dict(yaml_content))
 
-# for x in li:
-#    print(x.keys())
-#    print(x.values())
-
 headers = ['name', 'publisher', 'CORE', 'Scope', 'Short pages', 'Full pages', 'Format', 'CFP']
 
-# prepare markdown
 markdown_table = "| " + " | ".join(headers) + " |\n"
 markdown_table += "| " + " | ".join(["---"] * len(headers)) + " |\n"
 
@@ -26,8 +21,6 @@ for x in li:
                     v = str(v)
                 markdown_table += v + " | "
         markdown_table += "\n"
-
-print(markdown_table)
 
 with open('generated.md', 'w') as markdown_file:
     markdown_file.write(markdown_table)
