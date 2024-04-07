7568340f20bb3078279914fc25d3ffbfc9a79635
uchitsa <uchitsa@ya.ru>
2024-04-07 03:51:22 +0300

    update compile.py for generate md

--- compile.py
+++ compile.py
@@ -5,9 +5,28 @@ with open('cfp.yml', 'r') as stream:
     yaml_content = yaml.safe_load(stream)
     li.append(dict(yaml_content))
 
+header = '''# Curated List of CFPs
+
+This is a curated list of currently open Calls for Papers for computer
+science conferences. You are welcome to make changes and suggest conferences
+(and journals) that deserve inclusion. Obviously, our list is not complete
+and is not intended to be. For a larger and more complete list of
+currently open CFPs,
+you may check the [WikiCFP](http://www.wikicfp.com/cfp/) and
+[call4paper](https://www.call4paper.com/) websites.
+
+Our job is to regularly update the last column in the list, ensuring that
+you don't need to visit all conference websites. If you want your favorite
+conference to be included so you won't miss its deadline,
+please send us a pull request.
+'''
+
+markdown_table = header
+markdown_table += "\n"
+
 headers = ['name', 'publisher', 'CORE', 'Scope', 'Short pages', 'Full pages', 'Format', 'CFP']
 
-markdown_table = "| " + " | ".join(headers) + " |\n"
+markdown_table += "| " + " | ".join(headers) + " |\n"
 markdown_table += "| " + " | ".join(["---"] * len(headers)) + " |\n"
 
 for x in li:
