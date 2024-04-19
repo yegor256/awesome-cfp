654e4ef22b09dd473b22418451e1beb3e27ad5e9
uchitsa <uchitsa@ya.ru>
2024-04-20 02:58:34 +0300

    Update test-compile.py for correct generate md

--- test-compile.py
+++ test-compile.py
@@ -35,8 +35,30 @@ def generate_yaml():
 
 def generate_md():
     header = "# Curated List of CFPs\n"
+    header = '''# Curated List of CFPs
+This is a curated list of currently open Calls for Papers for computer
+science conferences. You are welcome to make changes and suggest conferences
+(and journals) that deserve inclusion. Obviously, our list is not complete
+and is not intended to be. For a larger and more complete list of
+currently open CFPs,
+you may check the [WikiCFP](http://www.wikicfp.com/cfp/) and
+[call4paper](https://www.call4paper.com/) websites.
+Our job is to regularly update the last column in the list, ensuring that
+you don't need to visit all conference websites. If you want your favorite
+conference to be included so you won't miss its deadline,
+please send us a pull request.\n'''
     sep = "<!-- events -->"
-    bottom = "**Some anagrams** in text."
+    bottom = '''
+**SE** stands for "Software Engineering",
+**SA** for "Software Architecture",
+**PL** for "Programming Languages",
+**ST** for "Software Testing".
+**SRC** stands for "Student Research Competition," where they _usually_ expect
+2-pages papers (sometimes up to three pages)
+from a single student, who must physically attend the event.
+**NIER** stands for "New Ideas and Emerging Results," where
+they _usually_ expect 4-pages papers.
+**Format** should be either LNCS, 2C (two columns), or 1C (one column).'''
     md_content = header + sep + "\n" + sep + bottom
     with open("test.md", "w") as f:
         f.write(md_content)
