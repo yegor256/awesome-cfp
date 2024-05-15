a6004832c595163715637f1a03e29449a9455974
uchitsa <uchitsa@ya.ru>
2024-05-16 02:12:41 +0300

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -3,10 +3,8 @@
 for dir in fixtures/*
 do
   mkdir temp
-  cp "$dir/input.yml" temp/cfp.yml
-  cp "$dir/input.md" temp/README.md
-  python3 compile.py temp/cfp.yml temp/README.md
-  diff "$dir/expected.md" temp/README.md
+  python3 compile.py "$dir/input.yml" "$dir/README.md"
+  diff "$dir/expected.md" "$dir/README.md"
   if [ $? -eq 0 ]; then
     echo "Test passed for $dir"
   else
